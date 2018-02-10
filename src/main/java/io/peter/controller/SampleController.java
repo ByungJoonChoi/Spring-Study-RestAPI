1ec47061e75405548f048609555628e1cdda3509
BJ CHOI <cbj0618@gmail.com>
2018-02-10 16:13:56 +0900

    <주요 내용 : 예외 발생시 상태코드 및 데이터 전송 방법> - ResponseEntity 객체를 사용하면 클라이언트에 HTTP 상태 코드를 전송할 수 있다. - HTTP 상태코드 뿐만 아니라 데이터도 전송가능 (주로 요청 실패에 대한 원인을 작성하여 전송)

--- src/main/java/io/peter/controller/SampleController.java
+++ src/main/java/io/peter/controller/SampleController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,5 +57,23 @@ public class SampleController {
 			map.put(i, vo);
 		}
 		return map; // Json Object 리턴 (key, value)
+	}
+	
+	@RequestMapping("/sendErrorAuth")
+	public ResponseEntity<Void> sendListAuth(){
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // HTTP status code(400) 전송
+	}
+	
+	@RequestMapping("/sendErrorNot")
+	public ResponseEntity<List<SampleVO>> sendListNot(){
+		List<SampleVO> list = new ArrayList<>();
+		for(int i=0 ; i<10 ; i++){
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("길동");
+			vo.setLastName("홍");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		return new ResponseEntity<>(list, HttpStatus.NOT_FOUND); // HTTP status code(404)와 데이터를 동시에 전송
 	}
 }
